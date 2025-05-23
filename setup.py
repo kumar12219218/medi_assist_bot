70d03ce6aae9c0ae5a942a907efcc1a5cfb2e22c
Bharath <bharathreddy.reegatipalli@gmail.com>
2025-05-23 22:25:51 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Bharath R',
+    author_email='bharathreddy.reegatipalli@gmail.com',
+    packages=find_packages(),
+    install_requires=[] 
+)
